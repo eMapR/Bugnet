cad180c895025647ddc4708bc81d7c46c1c0611d
clarype <clarype@oregonstate.edu>
2023-06-12 09:24:34 -0700

    added agent labeling scrtips

---
+++ scripts/04_agent_labeling/01_make_bugnet_polygons.js
@@ -0,0 +1,13 @@
+//Convert Bugnet Rasters into polygons. 
+//bugent config file 
+var bnet = require('users/clarype/DNR_WA:option2/config_option2_2022.js')
+
+//bunget image
+var image = ee.Image(bnet.param.assetDir+bnet.param.predicted);
+
+// image processing - reproject the image >> reduce pixel to polygons 
+var bnet_vects = image.reproject({crs:'EPSG:3857',scale:30}).reduceToVectors({reducer:ee.Reducer.countEvery(), scale:30, geometryType:"Polygon", eightConnected:true, labelProperty:"classification",maxPixels:1e13})
+Map.addLayer(bnet_vects)
+
+// export polygons as table
+Export.table.toAsset({collection:bnet_vects, description:bnet.param.bugnet_polygons, assetId:bnet.param.assetDir+bnet.param.bugnet_polygons})
---
+++ scripts/04_agent_labeling/02_label_bugnet_polygons_with_distance_image.js
@@ -0,0 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Extract distance raster values to points in bugnet polygons using sample regions then join the points 
+//distance attributes to the Bugnet polygons. 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//bugent config file 
+var bnet = require('users/clarype/DNR_WA:option2/config_option2_2022.js')
+
+// assets 
+var bunget_polygons = ee.FeatureCollection(bnet.param.assetDir+bnet.param.bugnet_polygons),
+    agent_distance_img = ee.Image(bnet.param.assetDir+bnet.param.bugnet_distance_img);
+
+// apply min pixel value to Bugnet polygon
+var labeled_bugnet = agent_distance_img.reduceRegions({collection:bunget_polygons, reducer:ee.Reducer.min(), scale:30, tileScale:8})
+
+// export 
+Export.table.toAsset({collection:labeled_bugnet, description:bnet.param.bugnet_polygons_labeled, assetId:bnet.param.assetDir+bnet.param.bugnet_polygons_labeled})
+

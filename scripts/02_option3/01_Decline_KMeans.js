f6356942d05e389eadca28e8e90946bb0566ef55
clarype <clarype@oregonstate.edu>
2023-06-11 19:08:30 -0700

    Found bug that is casuing problems with the sample to train Kmeans. I cant sample a heavily masked image. I am trying unmasking and filtering the sample.

--- scripts/02_option3/01_Decline_KMeans.js
+++ scripts/02_option3/01_Decline_KMeans.js
@@ -1,4 +1,5 @@
 // option 3 -- This program applies the KMeans clustering process on pixels decining with time.
+// option 3 -- This program applies the KMeans clustering process on pixels decining with time.
 //////////////////////////////////////////////MOD ASSETS///////////////////////////////////////////////////////////////
 //bugnet module
 var bugnet = require('users/clarype/DNR_WA:/support/bugnet.js'); 
@@ -7,11 +8,17 @@ var bugnet = require('users/clarype/DNR_WA:/support/bugnet.js');
 var bnet = require('users/clarype/DNR_WA:option2/config_option3_2022.js')
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // get reference image
-var ltsd_decline = ee.Image(bnet.param.assetDir+bnet.param.declineName)
+var ltsd_decline = ee.Image(bnet.param.assetDir+bnet.param.declineName).selfMask()
+Map.addLayer(ltsd_decline)
 // get list of bands from reference image
 var ltsd_bands = ltsd_decline.bandNames()
 
-var sample = ltsd_decline.sample({region:bnet.param.aoi, scale:30, numPixels: bnet.param.kmeans_num_sample, tileScale:4, geometries:true})
+//var sample = ltsd_decline.selfMask().sample({region:bnet.param.aoi, scale:30, numPixels: bnet.param.kmeans_num_sample, tileScale:4, geometries:true})
+var sample = ltsd_decline.unmask().sampleRegions({collection:ee.FeatureCollection(bnet.param.aoi), scale:30}).filter(ee.Filter.gt('yod',bnet.param.ltstartYear))
+
+Map.addLayer(sample)
+
+print(sample.size())
 
 var training = ee.Clusterer.wekaCascadeKMeans({
   minClusters:bnet.param.num_of_clusters,
@@ -22,11 +29,9 @@ var training = ee.Clusterer.wekaCascadeKMeans({
   inputProperties:ltsd_bands
 });
 
-var ltsd_decline_masked = ltsd_decline.mask(bnet.param.Mask)
+var ltsd_decline_masked = ltsd_decline.selfMask()
 
 var ltsd_decline_kmeans = ltsd_decline_masked.cluster(training).clip(bnet.param.aoi)
 
 Export.image.toAsset({image:ltsd_decline_kmeans.toInt16(), description:bnet.param.kmeansName, assetId:bnet.param.assetDir+bnet.param.kmeansName,region:bnet.param.aoi, scale:30, maxPixels:1000000000})
-
-
 

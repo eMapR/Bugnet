e00808f0fe3b4e40be04a71cce7e3742b5264afe
Peter C <clarype@oregonstate.edu>
2023-06-16 14:14:58 -0700

    added more parameters to the config file.

--- scripts/00_LT_change_predict/config.py
+++ scripts/00_LT_change_predict/config.py
@@ -1,7 +1,9 @@
 param=dict(
-	path=r"C:\Users\clary\Documents\bugnet\Bugnet",
-    change_type = ['disturbance'], # What change do you want to map (disturbance or growth)?
-    minMags = [100], #What is the desired minimum change magnitude:
-    collapseEm = [0], # Consecutive change segment collapse threshold (0 to ignore)
-    dsnrs = ['no'] #minimum change magnitude unit DSNR? (yes or no)
+	path=r"E:\v1\bugnet\workspace\Bugnet", # path to your project directory
+    change_type = ['disturbance'],                  # What change do you want to map (disturbance or growth)?
+    minMags = [100],                                # What is the desired minimum change magnitude:
+    collapseEm = [0],                               # Consecutive change segment collapse threshold (0 to ignore)
+    dsnrs = ['no'],                                 # minimum change magnitude unit DSNR? (yes or no)
+    mmu = [9],                                      # the minumum grouping of pixels to form polygons
+    connectednesses = ['yes']                       # Should diagonal adjacency warrant pixel inclusion in patches
     )

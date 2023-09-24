f5e6a67223bc8dbdf6d8bce24c78eae7b48b13dd
Clary <clarype@oregonstate.edu>
2023-09-24 15:53:48 -0700

    clean up

--- scripts/01_option_setup/10_make_forest_mask.js
+++ scripts/01_option_setup/10_make_forest_mask.js
@@ -1,18 +1,20 @@
+var mtbs = ee.FeatureCollection("USFS/GTAC/MTBS/burned_area_boundaries/v1")
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //bugnet module
-var bugnet = require('users/clarype/DNR_WA:/support/bugnet.js'); 
-var bnet = require('users/clarype/DNR_WA:option2/config_option2_2022.js')
-var lt_change = require('users/clarype/DNR_WA:/support/highMagChange.js')
+var bugnet = require('users/clarype/Bugnet_Blues_2023:bugnet.js'); 
+var bnet = require('users/clarype/Bugnet_Blues_2023:option3/config_option3.js')
+
 print(bnet)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // LCMS forest mask
 var lcms_mask = bugnet.lcms_forest_mask(bnet.param.target-5,bnet.param.target).clip(bnet.param.aoi)
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //reflectance mask
 var tassMap = bugnet.tasselCapMask(bnet)// <<<<<<<<<<<<<< need to find a better way. this works but ecoregions ...
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // High Magnitude -- makes a raster mask from vector layer of clear cuts fire etc 
-var highMagChange_img = lt_change.calc(bnet.param.target.toString()).reduceToImage(["yod"],ee.Reducer.mean()).gt(0).unmask().not()
+var highMagChange_img = bugnet.ltcalc(bnet.param.target.toString(),bnet.param.ltchange).reduceToImage(["yod"],ee.Reducer.mean()).gt(0).unmask().not()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Fire mask - filter MTBS dataset by date 
 var fires = mtbs.filter(ee.Filter.and(ee.Filter.gte("Ig_Date", bnet.param.maskStartTime), ee.Filter.lte("Ig_Date", bnet.param.maskEndTime) ))
@@ -25,3 +27,4 @@ var mask = lcms_mask.multiply(highMagChange_img).multiply(tassMap).multiply(fire
 // export image mask
 Export.image.toAsset({image:mask.byte(), description:bnet.param.forestMaskName, assetId:bnet.param.forestMaskName, region:bnet.param.aoi, scale:30, maxPixels:1e13})
 
+Map.addLayer(mask)
--- scripts/old/00_LT_change_predict/config.py
+++
@@ -1,3 +0,0 @@
-param=dict(
-	path=r"E:\v1\bugnet\workspace\Bugnet",
-	disturbance_mag =100)

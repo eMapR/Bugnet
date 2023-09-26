249a50010132fc9d833bc5011db9fd8b083aa674
Clary <clarype@oregonstate.edu>
2023-09-26 13:00:26 -0700

    updated script error

--- scripts/02_option3/00_LTSD_Decline.js
+++ scripts/02_option3/00_LTSD_Decline.js
@@ -5,11 +5,11 @@ var ltgee = require('users/emaprlab/public:Modules/LandTrendr.js');
 //bugnet module
 var bugnet = require('users/emaprlab/public:Modules/bugnet.js'); 
 //Configuation file
-var bnet = require('users/emaprlab/bugnetBlueMts2023:config2.js')
+var bnet = require('users/emaprlab/bugnetBlueMts2023:config3.js')
 
-print(ee.Image(bnet.param.LTSD))
+print(ee.Image(bnet.param.LTSDdir+bnet.param.LTSDname))
 //////////////////////////////////////////////LTSD SNIC DECLINE///////////////////////////////////////////////////////////////
-var ltsd_decline = bugnet.LTSD_decline_image(ee.Image(bnet.param.LTSD),bnet.param.target).mask(bnet.param.Mask)
+var ltsd_decline = bugnet.LTSD_decline_image(ee.Image(bnet.param.LTSDdir+bnet.param.LTSDname),bnet.param.target).mask(bnet.param.Mask)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Export.image.toAsset({image:ltsd_decline.toInt16(), description:bnet.param.declineName, assetId:bnet.param.assetDir+bnet.param.declineName,region:bnet.param.aoi, scale:30, maxPixels:1000000000})
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
